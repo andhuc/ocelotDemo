de81332e3149ba4eaf7bd00be868003037f0161d
anhduc2337 <anhduc2337@gmail.com>
2023-12-05 14:47:54 +0700

    init test

---
+++ APIGateway/Program.cs
@@ -0,0 +1,32 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+builder.Services.AddOcelot(builder.Configuration);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerForOcelot(builder.Configuration);
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseEndpoints(endpoints => {  endpoints.MapControllers(); });
+    app.UseSwaggerForOcelotUI().UseOcelot().Wait();
+    /*app.UseSwaggerUI();*/
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ Test.Serivce/Controllers/TestController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Test.Serivce.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TestController : Controller
+    {
+        [HttpGet]
+        public String Get()
+        {
+            return "oke";
+        }
+
+        [HttpGet("Details")]
+        public String GetDetails()
+        {
+            return "Details data";
+        }
+
+        [HttpGet("Oke")]
+        public String GetOke()
+        {
+            return "oke";
+        }
+    }
+}
