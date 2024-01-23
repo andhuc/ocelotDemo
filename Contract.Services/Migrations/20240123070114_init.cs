447e0ddf28ff806e9959524b59b89077bb38781a
andhuc <anhduc2337@gmail.com>
2024-01-23 14:02:43 +0700

    add migration

---
+++ Contract.Services/Migrations/20240123070114_init.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Contract.Service.Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "contracts",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    title = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    path = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    created_at = table.Column<DateTime>(type: "timestamp without time zone", nullable: true, defaultValueSql: "CURRENT_TIMESTAMP"),
+                    isSigned = table.Column<bool>(type: "boolean", nullable: true, defaultValueSql: "false")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_contracts", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "signature",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    contractid = table.Column<int>(type: "integer", nullable: false),
+                    x = table.Column<int>(type: "integer", nullable: false),
+                    y = table.Column<int>(type: "integer", nullable: false),
+                    width = table.Column<int>(type: "integer", nullable: false),
+                    height = table.Column<int>(type: "integer", nullable: false),
+                    name = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    reason = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    page = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_signature", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "contracts");
+
+            migrationBuilder.DropTable(
+                name: "signature");
+        }
+    }
+}
